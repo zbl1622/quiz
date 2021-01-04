84497d08d04f1529e615fe08758f1862b9619269
zhangbaoliang <zbl1622@gmail.com>
2021-01-04 10:03:43 +0800

    solve 509

---
+++ src/main/java/com/leetcode/quiz/Problem509.java
@@ -0,0 +1,64 @@
+package com.leetcode.quiz;
+
+import java.util.HashMap;
+
+/**
+ * 509. 斐波那契数
+ * <p>
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * <p>
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= n <= 30
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fibonacci-number
+ */
+public class Problem509 {
+
+    public static void main(String... args) {
+        int n = 4;
+
+        System.out.println(new Problem509().fib(n));
+    }
+
+    public static final HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            Integer r = hashMap.get(n);
+            if (r == null) {
+                r = fib(n - 1) + fib(n - 2);
+                hashMap.put(n, r);
+            }
+            return r;
+        }
+    }
+}
