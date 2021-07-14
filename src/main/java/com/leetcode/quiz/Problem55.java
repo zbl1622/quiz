b5714f6a0f6e3b905b8b671c4e79ec3c7f68c3de
zhangbaoliang <zbl1622@gmail.com>
2021-07-14 16:21:53 +0800

    solve 55

---
+++ src/main/java/com/leetcode/quiz/Problem55.java
@@ -0,0 +1,56 @@
+package com.leetcode.quiz;
+
+/**
+ * 55. 跳跃游戏
+ * 给定一个非负整数数组nums ，你最初位于数组的 第一个下标 。
+ * <p>
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * <p>
+ * 判断你是否能够到达最后一个下标。
+ * <p>
+ * <p>
+ * <p>
+ * 示例1：
+ * <p>
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ * <p>
+ * <p>
+ * 示例2：
+ * <p>
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 10^4
+ * 0 <= nums[i] <= 10^5
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jump-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Problem55 {
+
+    public static void main(String... args) {
+        int[] array = {2, 3, 1, 1, 4};
+//        int[] array = {3, 2, 1, 0, 4};
+
+        System.out.println(new Problem55().canJump(array));
+    }
+
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return max >= nums.length - 1;
+    }
+}
