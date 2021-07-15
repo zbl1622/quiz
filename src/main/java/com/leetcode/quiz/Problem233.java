89edca0238b9fc238bca6173d58728c18d2c0545
zhangbaoliang <zbl1622@gmail.com>
2021-07-15 20:53:00 +0800

    solve 233

--- src/main/java/com/leetcode/quiz/Problem233.java
+++ src/main/java/com/leetcode/quiz/Problem233.java
@@ -29,23 +29,56 @@ public class Problem233 {
 
     public static void main(String... args) {
 
-        System.out.println(new Problem233().countDigitOne(1120));
+        System.out.println(new Problem233().countDigitOne(1231));
     }
 
+    //聪明办法
     public int countDigitOne(int n) {
-        int s = 0;
-        for (int i = 1; i <= n; i++) {
-            int x = i;
-            int count = 0;
-            while (x > 0) {
-                int r = x % 10;
-                if (r == 1) {
-                    count += 1;
-                }
-                x = (x - r) / 10;
-            }
-            s += count;
+        long[] g = new long[9];
+        int x = 1, b = 1;
+        for (int i = 0; i < g.length; i++) {
+            g[i] = x;
+            b *= 10;
+            x = b + 10 * x;
         }
-        return s;
+        long s = 0;
+        int m = n;
+        int i = 0;
+        b = 1;
+        int r = 0;
+        while (m > 0) {
+            x = m % 10;
+            m /= 10;
+            r += b * x;
+            if (x == 1) {
+                s += r - b + 1;
+            } else if (x > 1) {
+                s += b;
+            }
+            if (i > 0) {
+                s += x * g[i - 1];
+            }
+            b *= 10;
+            i += 1;
+        }
+        return (int) s;
     }
+
+//    //笨办法
+//    public int countDigitOne(int n) {
+//        int s = 0;
+//        for (int i = 1; i <= n; i++) {
+//            int x = i;
+//            int count = 0;
+//            while (x > 0) {
+//                int r = x % 10;
+//                if (r == 1) {
+//                    count += 1;
+//                }
+//                x = (x - r) / 10;
+//            }
+//            s += count;
+//        }
+//        return s;
+//    }
 }
