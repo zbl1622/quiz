5261c274ea25c64820ac023c15722f4cb131aff2
zhangbaoliang <zbl1622@gmail.com>
2021-08-12 17:39:15 +0800

    solve 1143

---
+++ src/main/java/com/leetcode/quiz/Problem1143.java
@@ -0,0 +1,68 @@
+package com.leetcode.quiz;
+
+/**
+ * 1143. 最长公共子序列
+ * 给定两个字符串text1 和text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * <p>
+ * 一个字符串的子序列是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * <p>
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= text1.length, text2.length <= 1000
+ * text1 和text2 仅由小写英文字符组成。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-common-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Problem1143 {
+
+    public static void main(String... args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+
+        System.out.println(new Problem1143().longestCommonSubsequence(text1, text2));
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] chars1 = text1.toCharArray();
+        char[] chars2 = text2.toCharArray();
+        int[][] dp = new int[chars1.length + 1][chars2.length + 1];
+        for (int i = 0; i < chars1.length; i++) {
+            for (int j = 0; j < chars2.length; j++) {
+                int x = i + 1;
+                int y = j + 1;
+                if (chars1[i] == chars2[j]) {
+                    dp[x][y] = dp[x - 1][y - 1] + 1;
+                } else {
+                    dp[x][y] = Math.max(dp[x - 1][y], dp[x][y - 1]);
+                }
+            }
+        }
+        return dp[chars1.length][chars2.length];
+    }
+}
